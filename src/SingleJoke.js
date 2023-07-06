d450b551bcb32b109e254fc4a2ffe43a6cd723e2
ARoble <abdulladif0042@gmail.com>
2023-07-06 22:43:08 +0300

    inital commit

---
+++ src/SingleJoke.js
@@ -0,0 +1,7 @@
+function SingleJoke({data}){
+    return  <div className="content">
+    <h2 className="joke">{data.joke}</h2>
+ </div>
+}
+
+export default SingleJoke
---
+++ src/TwoPart.js
@@ -0,0 +1,9 @@
+function TwoPart({data,tellMe}){
+    return  <div className="content">
+    <h2 className="joke">{data.setup}</h2>
+    <h2 className="punch-line">{data.delivery}</h2>
+   
+ </div>
+}
+
+export default TwoPart
